3ffb4bb931c67efdba9d9b9df0cacb1e9a83e0b1
Hieu209Cnttqtk22a <dtc235210023@ictu.edu.vn>
2026-02-05 08:26:45 +0700

    Update Code

---
+++ MLTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using System.Threading.Tasks;
+using Google.GenAI;
+using Google.GenAI.Types;
+namespace MLTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "Youtube is so awful."
+            };
+
+            // Load model and predict output of sample data
+            var result = SentimentModel.Predict(sampleData);
+
+            // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+    }
+}
