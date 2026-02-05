c61509aff78f0c5958f07ef7bf6aad2eb851006c
Hieu209Cnttqtk22a <dtc235210023@ictu.edu.vn>
2026-02-05 08:45:10 +0700

    testh

--- MLTest/Program.cs
+++ MLTest/Program.cs
@@ -16,6 +16,7 @@ namespace MLTest
             // Load model and predict output of sample data
             // quang khai báo
             // nguyen test
+            // test hieu
             var result = SentimentModel.Predict(sampleData);
 
             // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
